3dccc62bde2b70e45ae30b26c5fa86cdc36821f1
Ashif091 <fhyvhh091@gmail.com>
2024-09-20 10:45:57 +0530

    working on text editor

--- src/components/workspace/document-detail .tsx
+++ src/components/workspace/document-detail .tsx
@@ -1,22 +1,44 @@
 "use client"
-import dynamic from "next/dynamic";
+import dynamic from "next/dynamic"
 // import Editor from "./Editor";
- 
-// const Editor = dynamic(() => import("./Editor"), { ssr: false });
 
+const Editor = dynamic(() => import("./Editor"), {ssr: false})
 
-interface DocumentDetailProps {
-  document: any
-}
+// interface DocumentDetailProps {
+//   document: any
+// }
 
-const DocumentDetail: React.FC<DocumentDetailProps> = ({document}) => {
+// const DocumentDetail: React.FC<DocumentDetailProps> = ({document}) => {
+//   return (
+//     <div className="p-4">
+//       <h2 className="text-2xl font-semibold mb-4 cursor-pointer">{document.title}</h2>
+//       <p>Document content goes here...</p>
+//       {/* <Editor /> */}
+//     </div>
+//   )
+// }
+
+// export default DocumentDetail
+
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense"
+// import { Editor } from "./Editor";
+
+export default function DocumentDetail({document}:any) {
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-semibold mb-4 cursor-pointer">{document.title}</h2>
-      <p>Document content goes here...</p>
-      {/* <Editor /> */}
-    </div>
+    <LiveblocksProvider publicApiKey={"pk_dev_tZngn…c7iL6R"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          <h2 className="text-2xl font-semibold mb-4 cursor-pointer">
+            {document.title}
+          </h2>
+          <p>Document content goes here...</p> 
+          <Editor />
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
   )
-} 
-
-export default DocumentDetail
+}
