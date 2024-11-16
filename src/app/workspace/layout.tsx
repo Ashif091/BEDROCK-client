a892e7e72e1d67df055a5d424cb41fa5c11560da
Ashif091 <fhyvhh091@gmail.com>
2024-11-16 09:41:46 +0530

    fix:live socket connection

--- src/app/workspace/layout.tsx
+++ src/app/workspace/layout.tsx
@@ -18,15 +18,16 @@ const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
   const socketRef = useRef<any>(null)
   useEffect(() => {
     if (!socketRef.current) {
-      socketRef.current = io(process.env.NEXT_PUBLIC_SERVER_URL!, {
-        path: '/app/socket.io',
+      socketRef.current = io(process.env.NEXT_PUBLIC_SERVER_URL)
+      // , {
+        // path: '/app/socket.io',
         // transports: ['websocket', 'polling'],
-        secure:false,
+        // secure:false,
         // rejectUnauthorized: false,
         // reconnection: true,
         // reconnectionAttempts: 5,
         // reconnectionDelay: 1000,
-      })
+      // }
     }
     if (user?.email) {
       socketRef.current.emit("notify", user.email)
